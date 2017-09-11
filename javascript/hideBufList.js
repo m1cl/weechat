0713bda3a6c8f2f6f7d816635cd8fe1840df0433
m1cl <michael_ajala@yahoo.de>
2017-09-11 12:41:48 +0200

    first

---
+++ javascript/hideBufList.js
@@ -0,0 +1,10 @@
+let name = 'hideBufList'
+let author = 'michael_ajala'
+let version = '1.0'
+let license = 'MIT'
+let description = 'toggle/hide bufferlist'
+let shutdown = ''
+let charset = 'UTF-8'
+
+weechat.register(name, author, version, license,description, shutdown, charset )
+weechat.print("Hello World")
